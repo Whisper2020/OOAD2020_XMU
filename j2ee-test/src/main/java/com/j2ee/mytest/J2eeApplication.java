1210e94dfbefe4af2ff6b173f5da2a691cf245fb
Snow02203835 <snow02203835@163.com>
2020-11-10 15:00:35 +0800

    添加依赖并增加MapperScan

--- j2ee-test/src/main/java/com/j2ee/mytest/J2eeApplication.java
+++ j2ee-test/src/main/java/com/j2ee/mytest/J2eeApplication.java
@@ -1,9 +1,11 @@
 package com.j2ee.mytest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.j2ee.mytest.mapper")
 public class J2eeApplication {
 
     public static void main(String[] args) {
